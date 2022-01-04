b62f5a5347b3b318e398bc4a15349a0eed0397d8
Seungjoon Lee <43627973+leesj1991@users.noreply.github.com>
2022-01-04 17:15:35 +0900

    Add files via upload

---
+++ RNAseq_visualization_20220104v1.R
@@ -0,0 +1,286 @@
+#결과에 책임지지 않습니다. Raw data와 충분히 교차검증 하고 사용하세요.
+
+rm(list = ls(all = TRUE))
+setwd(choose.dir(default = "", caption = "Select folder"))
+
+#Packages
+{
+  if(require(readxl)){
+  } else {install.packages(readxl)}
+  
+  if(require(dplyr)){
+  } else {install.packages(dplyr)}
+  
+  if(require(janitor)){
+  } else {install.packages(janitor)}
+  
+  if(require(ggplot2)){
+  } else {install.packages(janitor)}
+  
+  if(require(stringr)){
+  } else {install.packages(stringr)}
+  
+  if(require(tictoc)){
+  } else {install.packages(tictoc)}
+}
+
+tic()
+
+data = data.frame()
+
+file_list = list.files(pattern = "xlsx")
+
+for (file_name in file_list){
+  
+  sheet_list = excel_sheets(file_name)
+  
+  for (sheet_name in sheet_list){
+    
+    temp_data = read_excel(file_name, sheet = sheet_name)
+    if (nrow(temp_data) == 0) next
+    
+    temp_file_name = strsplit(file_name,".",1)[[1]][1]
+
+#-># File name parcing #########################################################
+    temp_file_name_parse = strsplit(temp_file_name,"_")[[1]]
+    
+    temp_data = bind_cols(temp_data, AGE = temp_file_name_parse[3])
+    temp_data = bind_cols(temp_data, SEX = temp_file_name_parse[1])
+    temp_data = bind_cols(temp_data, GENOTYPE = temp_file_name_parse[4])
+    temp_data = bind_cols(temp_data, DIRECTION = temp_file_name_parse[5])
+    temp_data = bind_cols(temp_data, GENESET = sheet_name)
+    temp_data = bind_cols(temp_data, GROUP = paste(temp_file_name_parse[3], temp_file_name_parse[4]))
+#<-#############################################################################
+    
+    data = bind_rows(data, temp_data)
+  }
+}
+
+data = data %>% clean_names()
+save(data, file = "totaldata_total.rdata")
+
+data = data %>% filter(fdr_q_val < 0.05)
+save(data, file = "totaldata_onlySignificant.rdata")
+
+rm(list = ls(all = TRUE))
+
+#-># Maximum Y axis number #####################################################
+maximum_geneset = 30
+#<-#############################################################################
+
+defaultW <- getOption("warn") 
+options(warn = -1) 
+
+#Draw total data
+load("totaldata_total.rdata")
+pdf("GSEA_plot_total.pdf", paper="a4")
+{
+  #variables
+  group_list = unique(data$group)
+  geneset_list = unique(data$geneset)
+  direction_list = rev(unique(data$direction))
+  bgColorForExport = c("pink", "lightblue")
+  
+  for(choose_geneset in geneset_list){
+    for(change_direction in direction_list){
+      for(arrby in group_list){
+        
+        arrby_data = data %>% filter(group == arrby & geneset == choose_geneset)
+        if(nrow(arrby_data) == 0) next
+        tempdata = data[which(data$name %in% arrby_data$name),] %>% filter(geneset == choose_geneset)
+        
+        plot_number = min(maximum_geneset, nrow(arrby_data))
+        
+        if(change_direction == direction_list[1]){
+          tempdata = tempdata %>% arrange(desc(nes))
+        }else if(change_direction == direction_list[2]){
+          tempdata = tempdata %>% arrange(nes)
+        }
+        
+        tempdata$name = chartr("_", " ", tempdata$name)
+        tempdata$name = str_wrap(tempdata$name, width = 30)
+        tempdata = tempdata %>% na.omit()
+        order = (tempdata %>% filter(group==arrby) %>% select(name))[1:plot_number,]
+        
+        s = ggplot(tempdata, aes(x = group, 
+                                 y = name, 
+                                 size = -log2(fdr_q_val+1e-10), 
+                                 color = nes)) + 
+          geom_point(alpha = 1) + 
+          theme_light() + 
+          ggtitle(str_c("Geneset: ", choose_geneset, 
+                        "\nChange direction: ", change_direction, 
+                        "\nArranged by: ", arrby)) + 
+          theme(panel.grid.major = element_line(size = 0.2, linetype = "solid", color = bgColorForExport[change_direction])) +
+          theme(axis.title.x = element_blank()) +
+          theme(axis.title.y = element_blank()) +
+          scale_y_discrete(limits = rev(order)) +
+          theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1)) +
+          geom_vline(xintercept = arrby, linetype = 'dashed', color='grey50', size = 0.2) +
+          #annotate("rect", xmin = 0.5, xmax = 1.5, ymin = 1, ymax = 20, alpha = 1, fill = "green") +
+          #geom_rect(inherit.aes=FALSE, aes(xmin=0.5, xmax=1.5, ymin=3, ymax=4), color="transparent", fill="orange", alpha=0.3) +
+          scale_color_gradient2(midpoint=0, low="blue", mid="white", high="red", space ="Lab" )
+        
+        print(s)
+      }
+    }
+  }
+  
+  dev.off()
+}
+
+rm(data)
+
+#Draw only significant data
+load("totaldata_onlySignificant.rdata")
+pdf("GSEA_plot_onlySignificant.pdf", paper="a4")
+{
+  #variables
+  group_list = unique(data$group)
+  geneset_list = unique(data$geneset)
+  direction_list = rev(unique(data$direction))
+  bgColorForExport = c("pink", "lightblue")
+  
+  for(choose_geneset in geneset_list){
+    for(change_direction in direction_list){
+      for(arrby in group_list){
+        
+        arrby_data = data %>% filter(group == arrby & geneset == choose_geneset)
+        if(nrow(arrby_data) == 0) next
+        tempdata = data[which(data$name %in% arrby_data$name),] %>% filter(geneset == choose_geneset)
+        
+        plot_number = min(maximum_geneset, nrow(arrby_data))
+        
+        if(change_direction == direction_list[1]){
+          tempdata = tempdata %>% arrange(desc(nes))
+        }else if(change_direction == direction_list[2]){
+          tempdata = tempdata %>% arrange(nes)
+        }
+        
+        tempdata$name = chartr("_", " ", tempdata$name)
+        tempdata$name = str_wrap(tempdata$name, width = 30)
+        tempdata = tempdata %>% na.omit()
+        order = (tempdata %>% filter(group==arrby) %>% select(name))[1:plot_number,]
+        
+        s = ggplot(tempdata, aes(x = group, 
+                                 y = name, 
+                                 size = -log2(fdr_q_val+1e-10), 
+                                 color = nes)) + 
+          geom_point(alpha = 1) + 
+          theme_light() + 
+          ggtitle(str_c("Geneset: ", choose_geneset, 
+                        "\nChange direction: ", change_direction, 
+                        "\nArranged by: ", arrby)) + 
+          theme(panel.grid.major = element_line(size = 0.2, linetype = "solid", color = bgColorForExport[change_direction])) +
+          theme(axis.title.x = element_blank()) +
+          theme(axis.title.y = element_blank()) +
+          scale_y_discrete(limits = rev(order)) +
+          theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1)) +
+          geom_vline(xintercept = arrby, linetype = 'dashed', color='grey50', size = 0.2) +
+          #annotate("rect", xmin = 0.5, xmax = 1.5, ymin = 1, ymax = 20, alpha = 1, fill = "green") +
+          #geom_rect(inherit.aes=FALSE, aes(xmin=0.5, xmax=1.5, ymin=3, ymax=4), color="transparent", fill="orange", alpha=0.3) +
+          scale_color_gradient2(midpoint=0, low="blue", mid="white", high="red", space ="Lab" )
+        
+        print(s)
+      }
+    }
+  }
+  
+  dev.off()
+}
+
+rm(data)
+
+#Draw most frequently used total data
+load("totaldata_total.rdata")
+pdf("GSEA_plot_most_frequently_used_total.pdf", paper="a4")
+{
+  #variables
+  group_list = unique(data$group)
+  #geneset_list = unique(data$geneset)
+  geneset_list = c("ASD_RISK", "Celltype", "Sub_celltype")
+  order = list(c("DEG UP VOINEAGU", "COEX UP M16 VOINEAGU", "DEG DOWN VOINEAGU", "COEX DOWN M12 VOINEAGU", "SFARIGENE", "SFARIGENE(HIGH CONFIDENCE)", "FMRPTARGETS", "DENOVOMISS", "DENOVOVARIANTS", "ASD AUTISMKB"), 
+               c("NEURONS CAHOY", "S1 PYRNEURONS ZEISEL", "CA1 PYRNEURONS ZEISEL", "INTERNEURONS ZEISEL", "OLIGODENDROCYTES CAHOY", "OLIGODENDROCYTES ZEISEL", "ASTROCYTES CAHOY", "ASTROCYTES ZEISEL", "MICROGLIA ZEISEL", "MICROGLIA ALBRIGHT", "ENDOTHELIAL ZEISEL"), 
+               c("CTX GLU LAYER1", "CTX GLU LAYER2-4", "CTX GLU LAYER4", "CTX GLU LAYER5", "CTX GLU LAYER6", "GABAPAN GAD1-2", "GABAPRO ASCL1", "GABAPRO DLX1-2", "GABAPRO NKX2-1", "GABA PVALB", "GABA CALB1", "GABA CALB2", "GABA CCK", "GABA NOS1", "GABA VIP", "OLIGODENDROCYTES PROGE", "OLIGODENDROCYTES MATURE", "ASTROCYTES", "MICROGLIA"))
+  
+  bgColorForExport = c("pink", "lightblue")
+  
+  for(choose_geneset in geneset_list){
+    
+    tempdata = data %>% filter(geneset == choose_geneset)
+    
+    tempdata$name = chartr("_", " ", tempdata$name)
+    tempdata$name = str_wrap(tempdata$name, width = 30)
+    tempdata = tempdata %>% na.omit()
+    
+    s = ggplot(tempdata, aes(x = group, 
+                             y = name, 
+                             size = -log2(fdr_q_val+1e-10), 
+                             color = nes)) + 
+      geom_point(alpha = 1) + 
+      theme_light() + 
+      ggtitle(str_c("Geneset: ", choose_geneset)) + 
+      theme(panel.grid.major = element_line(size = 0.2, linetype = "solid")) +
+      theme(axis.title.x = element_blank()) +
+      theme(axis.title.y = element_blank()) +
+      scale_y_discrete(limits = order[[which(geneset_list == choose_geneset)]]) +
+      theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1)) +
+      #annotate("rect", xmin = 0.5, xmax = 1.5, ymin = 1, ymax = 20, alpha = 1, fill = "green") +
+      #geom_rect(inherit.aes=FALSE, aes(xmin=0.5, xmax=1.5, ymin=3, ymax=4), color="transparent", fill="orange", alpha=0.3) +
+      scale_color_gradient2(midpoint=0, low="blue", mid="white", high="red", space ="Lab" )
+    
+    print(s)
+  }
+  
+  dev.off()
+}
+
+rm(data)
+
+#Draw most frequently used significant data
+load("totaldata_onlySignificant.rdata")
+pdf("GSEA_plot_most_frequently_used_significant.pdf", paper="a4")
+{
+  #variables
+  group_list = unique(data$group)
+  #geneset_list = unique(data$geneset)
+  geneset_list = c("ASD_RISK", "Celltype", "Sub_celltype")
+  order = list(c("DEG UP VOINEAGU", "COEX UP M16 VOINEAGU", "DEG DOWN VOINEAGU", "COEX DOWN M12 VOINEAGU", "SFARIGENE", "SFARIGENE(HIGH CONFIDENCE)", "FMRPTARGETS", "DENOVOMISS", "DENOVOVARIANTS", "ASD AUTISMKB"), 
+               c("NEURONS CAHOY", "S1 PYRNEURONS ZEISEL", "CA1 PYRNEURONS ZEISEL", "INTERNEURONS ZEISEL", "OLIGODENDROCYTES CAHOY", "OLIGODENDROCYTES ZEISEL", "ASTROCYTES CAHOY", "ASTROCYTES ZEISEL", "MICROGLIA ZEISEL", "MICROGLIA ALBRIGHT", "ENDOTHELIAL ZEISEL"), 
+               c("CTX GLU LAYER1", "CTX GLU LAYER2-4", "CTX GLU LAYER4", "CTX GLU LAYER5", "CTX GLU LAYER6", "GABAPAN GAD1-2", "GABAPRO ASCL1", "GABAPRO DLX1-2", "GABAPRO NKX2-1", "GABA PVALB", "GABA CALB1", "GABA CALB2", "GABA CCK", "GABA NOS1", "GABA VIP", "OLIGODENDROCYTES PROGE", "OLIGODENDROCYTES MATURE", "ASTROCYTES", "MICROGLIA"))
+  
+  bgColorForExport = c("pink", "lightblue")
+  
+  for(choose_geneset in geneset_list){
+    
+    tempdata = data %>% filter(geneset == choose_geneset)
+    
+    tempdata$name = chartr("_", " ", tempdata$name)
+    tempdata$name = str_wrap(tempdata$name, width = 30)
+    tempdata = tempdata %>% na.omit()
+    
+    s = ggplot(tempdata, aes(x = group, 
+                             y = name, 
+                             size = -log2(fdr_q_val+1e-10), 
+                             color = nes)) + 
+      geom_point(alpha = 1) + 
+      theme_light() + 
+      ggtitle(str_c("Geneset: ", choose_geneset)) + 
+      theme(panel.grid.major = element_line(size = 0.2, linetype = "solid")) +
+      theme(axis.title.x = element_blank()) +
+      theme(axis.title.y = element_blank()) +
+      scale_y_discrete(limits = order[[which(geneset_list == choose_geneset)]]) +
+      theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1)) +
+      #annotate("rect", xmin = 0.5, xmax = 1.5, ymin = 1, ymax = 20, alpha = 1, fill = "green") +
+      #geom_rect(inherit.aes=FALSE, aes(xmin=0.5, xmax=1.5, ymin=3, ymax=4), color="transparent", fill="orange", alpha=0.3) +
+      scale_color_gradient2(midpoint=0, low="blue", mid="white", high="red", space ="Lab" )
+    
+    print(s)
+  }
+  
+  dev.off()
+}
+
+options(warn = defaultW)
+
+toc()
